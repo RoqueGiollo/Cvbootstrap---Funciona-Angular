f4ebb9e4b7d39ebcf51854910d3ffc178b25fc46
Roque Giollo <giolloroque@gmail.com>
2022-06-27 01:36:18 -0300

    Cv Bootstrap - Funcionando en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestComponent } from './interest/interest.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
