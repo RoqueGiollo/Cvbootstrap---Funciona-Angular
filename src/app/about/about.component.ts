4b72c7410626a54f90056db6d9a2e3ff22b1bab3
Roque Giollo <giolloroque@gmail.com>
2022-06-29 22:05:37 -0300

    EDITOR DE TEXTO-OK

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+aboutText: string = 
+"Soy una persona que siempre fue apasionada por la tecnologia y la ciencia, esto me llevo a moverme en el rugbro desde joven. En principio realizaba reparacion de pc y comercializacion de componentes, luego sume redes. En estos momentos estoy estudiando Full Stack jr luego de haber ejercido un tiempo en ventas."
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getVal(val: string){   
+    this.aboutText=val 
+  
+  }
 }
